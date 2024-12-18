18aa49c853c56103ecdc603ca868fa2766ecf117
ekta784 <143435013+ekta784@users.noreply.github.com>
2024-12-18 18:40:15 +0530

    inheritance

---
+++ ch11oops2.py
@@ -0,0 +1,15 @@
+class Employee:
+    company="ITC"
+    def show(self):
+        print(f"my name is {self.name}")
+
+class Programmer(Employee):
+    company="ITC Infotech"
+    def show(self):
+        print(f"my name is {self.name}")
+    def showlanguage(self):
+        print(f"my name is {self.name} and language i know is {self.language}")
+a=Employee()
+b=Programmer()
+print(a.company,b.company)
+print(b.show)
