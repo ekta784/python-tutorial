898e7561eb68dd4d4aa73b5580096a3cea30217f
ekta784 <143435013+ekta784@users.noreply.github.com>
2025-01-01 18:59:43 +0530

    ch-13..

--- ch12-exception.py
+++ ch12-exception.py
@@ -12,3 +12,26 @@ if(b==0):
     raise ZeroDivisionError("programme is not ment to divide a number by zero")
 else:
     print(f"the divisioj is {a/b}")
+
+
+#try and else
+try:
+    a=int(input("enter a number:"))
+    print(a)
+except Exception as e:
+    print(e)
+else:
+    print("i'm inside else loop")
+
+#try and finally
+def main():
+    try:
+        a=int(input("enter a number:"))
+        print(a)
+        return
+    except Exception as e:
+        print(e)
+        return
+    finally:
+        print("i'm inside else loop")
+main()
---
+++ ch12-global.py
@@ -0,0 +1,8 @@
+a=89
+def fun():
+    #to make a globle use globle word
+    global a
+    a=3
+    print(a)
+fun()
+print(a)
