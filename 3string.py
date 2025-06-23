10846becc175f8ba35360679523f3dde966d519d
ekta784 <143435013+ekta784@users.noreply.github.com>
2025-06-23 18:10:48 +0530

    revised

--- 3string.py
+++ 3string.py
@@ -1,4 +1,9 @@
-# indexing in string always start with 0
-a= "ekta"
-b=a[0:2]  # strati with 0 end in 2 and don't include 2
-print(b)
+# # indexing in string always start with 0
+# a= "ekta"
+# b=a[0:2]  # strati with 0 end in 2 and don't include 2
+# print(b)
+
+c=10
+print(type(c))
+b=str(c)
+print(type(b))
